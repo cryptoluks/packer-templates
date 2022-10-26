8e21ca2f3d412d53cd4387b4e854ab38a7f35a21
cryptoluks <9020527+cryptoluks@users.noreply.github.com>
2022-10-26 11:12:48 +0200

    Init packer in case update plugin not already installed

--- windows/packer/build.sh
+++ windows/packer/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+\rm windows.box
 vboxmanage unregistervm --delete windows
 set -e
+packer init 01-updates.pkr.hcl
 packer build -var-file="secrets.auto.pkrvars.hcl" -force 01-updates.pkr.hcl
 packer build -var-file="secrets.auto.pkrvars.hcl" -force 02-updates2.pkr.hcl
 packer build -var-file="secrets.auto.pkrvars.hcl" -force 03-packages.pkr.hcl
